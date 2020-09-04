00ed2cc961b20384a36c2d3d3a715d4a0a6b4478
sharath2106 <gopi.shanky@gmail.com>
2020-09-04 10:12:28 +0530

    4th September 2020 - add gitignore

---
+++ src/main/java/practice/arrays/ProductOfArray.java
@@ -0,0 +1,14 @@
+package practice.arrays;
+
+public class SumOfArray {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7};
+        int sum = 0;
+
+        for(int arrayValue : array) {
+            sum+=arrayValue;
+        }
+
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/practice/arrays/SmallestElementInArray.java
@@ -0,0 +1,15 @@
+package practice.arrays;
+
+public class SmallestElementInArray {
+    public static void main(String[] args) {
+        int[] array = {4, 2, 6, 8, 9};
+        int smallestElement = 9999;
+
+        for (int arrayElement : array) {
+            if (smallestElement > arrayElement) {
+                smallestElement = arrayElement;
+            }
+        }
+        System.out.println(smallestElement);
+    }
+}
