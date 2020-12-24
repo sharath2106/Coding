651f4450ad6ff51cdf401fc00462bf97a266a947
sharath2106 <gopi.shanky@gmail.com>
2020-12-24 17:01:57 +0530

    add program to check if string contains all digits

---
+++ src/main/java/Day1/DoesStringContainsAllDigits.java
@@ -0,0 +1,35 @@
+package Day1;
+
+public class DoesStringContainsAllDigits {
+    public static void main(String[] args) {
+        String str = "Welcome123456789MyChannel";
+        DoesStringContainsAllDigits ds = new DoesStringContainsAllDigits();
+        if(ds.allDigitsPresent(str))
+            System.out.println("TRUE");
+        else
+            System.out.println("FALSE");
+    }
+
+    private boolean allDigitsPresent(String str) {
+        boolean[] bool = new boolean[10];
+        for (int i = 0; i < str.length(); i++) {
+            if (isDigit(str.charAt(i))) {
+                int digit = str.charAt(i) - '0';
+                bool[digit] = true;
+            }
+        }
+
+        for(int i =0; i<bool.length; i++) {
+            if(bool[i]==false)
+                return false;
+        }
+        return true;
+    }
+
+    private boolean isDigit(char charVal) {
+        if (charVal >= '0' && charVal <= '9')
+            return true;
+        else
+            return false;
+    }
+}
