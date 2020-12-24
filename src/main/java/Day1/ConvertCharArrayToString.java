905e264ef37033c6668fe60364db158c71f64e96
sharath2106 <gopi.shanky@gmail.com>
2020-12-24 12:44:07 +0530

    add method to convert char array using StringBuilder

--- src/main/java/Day1/ConvertCharArrayToString.java
+++ src/main/java/Day1/ConvertCharArrayToString.java
@@ -6,13 +6,20 @@ public class ConvertCharArrayToString {
     public static void main(String[] args) {
         ConvertCharArrayToString s1 = new ConvertCharArrayToString();
         s1.printStringFromCharArray();
+        s1.printStringFromCharArrayUsingStringBuilder();
+    }
+
+    private void printStringFromCharArrayUsingStringBuilder() {
+        char[] charArray = new char[]{'1', '0'};
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(charArray);
+        System.out.println("Appending character array using String builder " + stringBuilder);
     }
 
     private void printStringFromCharArray() {
         char[] charArray = new char[]{'1', '0'};
         System.out.println(charArray);
         String str = Arrays.toString(charArray);
-        String replacedString = str.replace("[", "");
-        System.out.println(replacedString);
+        System.out.println("Converting character array to String using Arrays.toString method " + str);
     }
 }
