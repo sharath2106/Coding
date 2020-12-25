3b453e8f6228bdf951921ed6bd6e83e4bd2b400f
sharath2106 <gopi.shanky@gmail.com>
2020-12-25 20:07:27 +0530

    add program for finding largest number in an array

---
+++ src/main/java/Day2/ReturnIndexLargestNumberOfArray.java
@@ -0,0 +1,19 @@
+package Day2;
+
+public class ReturnIndexLargestNumberOfArray {
+    public static void main(String[] args) {
+        ReturnIndexLargestNumberOfArray index = new ReturnIndexLargestNumberOfArray();
+        int[] elements = new int[]{5, 22, 3, 49};
+        System.out.println(index.largestNumberIndex(elements));
+    }
+
+    private int largestNumberIndex(int[] numbers) {
+        int MAX = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > MAX) {
+                MAX = numbers[i];
+            }
+        }
+        return MAX;
+    }
+}
