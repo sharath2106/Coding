69b62a34e1aeab5ef4f528d9ad67196a5c3d8756
sharath2106 <gopi.shanky@gmail.com>
2020-12-28 08:46:00 +0530

    add better solution for sherlock and valid string

--- src/main/java/Day4/SherlockAndValidString.java
+++ src/main/java/Day4/SherlockAndValidString.java
@@ -1,45 +1,36 @@
 package Day4;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class SherlockAndValidString {
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String input = sc.next();
 
-    Scanner scan = new Scanner(System.in);
-    String sherlockString = scan.nextLine();
-    Map<Character, Integer> countChar = new HashMap<>();
-    Map<Integer, Integer> countCount = new HashMap<>();
-    int length = sherlockString.length();
-    int i;
-    for (i = 0; i < length; i++) {
-      char c = sherlockString.charAt(i);
-      if (countChar.containsKey(c)) {
-        countChar.put(c, countChar.get(c) + 1);
+    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+    int frequency = 0;
+    int del = 0;
+
+    for (int i = 0; i < input.length(); i++) {
+      if (map.get(input.charAt(i)) == null) {
+        map.put(input.charAt(i), 1);
       } else {
-        countChar.put(c, 1);
+        map.put(input.charAt(i), map.get(input.charAt(i)) + 1);
+        frequency = map.get(input.charAt(i));
       }
     }
 
-    for (Map.Entry<Character, Integer> entry : countChar.entrySet()) {
-      if (countCount.containsKey(entry.getValue())) {
-        countCount.put(entry.getValue(), countCount.get(entry.getValue()) + 1);
-      } else {
-        countCount.put(entry.getValue(), 1);
+    for (char c : map.keySet()) {
+      if (frequency != map.get(c)) {
+        del++;
       }
     }
-    if (countCount.size() == 1) {
-      System.out.println("YES");
-    } else if (countCount.size() == 2) {
-      if (countCount.containsValue(1)) {
-        System.out.println("YES");
-      } else {
-        System.out.println("NO");
-      }
-    } else {
+    if (del > 1) {
       System.out.println("NO");
+    } else {
+      System.out.println("YES");
     }
   }
 }
