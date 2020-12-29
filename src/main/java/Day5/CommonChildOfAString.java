05eb4b9f9476d6d54da684626ca55d7a9b43bc00
sharath2106 <gopi.shanky@gmail.com>
2020-12-29 22:50:13 +0530

    add program for ReverseWordsOfAString

--- src/main/java/Day5/CommonChildOfAString.java
+++ src/main/java/Day5/CommonChildOfAString.java
@@ -3,23 +3,15 @@ package Day5;
 public class CommonChildOfAString { // Needs change in solution
   public static void main(String[] args) {
     String firstString = "abcde";
-    String secondString = "defgh";
+    String secondString = "fghyu";
     int firstStringLength = firstString.length();
     int[][] commonChars = new int[firstStringLength + 1][firstStringLength + 1];
     for (int i = 1; i <= firstStringLength; i++) {
       for (int j = 1; j <= firstStringLength; j++) {
-        System.out.println("i ---> " + i + "j ---> " + j);
         if (firstString.charAt(i - 1) != secondString.charAt(j - 1)) {
-          System.out.println("First string charat -----> " + firstString.charAt(i - 1));
-          System.out.println("Second string charat -----> " + secondString.charAt(j - 1));
-          System.out.println("FIRST LOOP 1----> " + commonChars[i][j - 1]);
-          System.out.println("FIRST LOOP 2 ----> " + commonChars[i - 1][j]);
           commonChars[i][j] = Math.max(commonChars[i][j - 1], commonChars[i - 1][j]);
-          System.out.println("MAX LOOP 1 ----> " + commonChars[i][j]);
         } else {
-          System.out.println("SECOND LOOP ---> " + commonChars[i - 1][j - 1]);
           commonChars[i][j] = commonChars[i - 1][j - 1] + 1;
-          System.out.println("MAX LOOP 2 ----> " + commonChars[i][j]);
         }
       }
     }
